b36cd49c2c348e33a779f0b2c0a02712753eb7e3
Loknath Bharti <lbharti@gmail.com>
2022-03-29 17:17:46 +0530

    some improvements

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import './test.scss'
+import './style.scss'
 
 class Main extends React.Component {
   render() {
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: './static'
   },
   plugins: [
     new CleanWebpackPlugin(),
