63e98affa12e63b03d58e8aaaf19e2b5e68ae155
Loknath Bharti <lbharti@gmail.com>
2022-09-03 08:19:05 +0530

    added babel wildcard support

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,8 @@ module.exports = {
             plugins: [
               ['@babel/plugin-proposal-object-rest-spread'],
               ['@babel/plugin-proposal-class-properties'],
-              ["@babel/plugin-transform-react-jsx"]
+              ["@babel/plugin-transform-react-jsx"],
+              ["babel-plugin-wildcard"]
             ]
           }
         }
